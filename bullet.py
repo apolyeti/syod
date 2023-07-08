7b68513088df4d61bfc69f6de0a3bf2a4b10edb4
apolyeti <azhandarveen@gmail.com>
2023-07-07 18:12:53 -0700

    added bullet functionality

---
+++ bullet.py
@@ -0,0 +1,41 @@
+import pygame 
+PLAYER_RADIUS = 5
+
+# make an object Bullet that is spawned when the player presses space
+# the bullet should have a position and a constant velocity
+# the bullet will bounce off the walls and only gets destroyed when it hits the player
+# the bullet should be a circle with a radius of 3
+# the bullet should be red
+# in terms of spawning the bullets, there will be an invisible cross hair that will be
+# spawned at the player's position. The bullet will spawn at the cross hair's position
+# this crosshair will rotate constantly clockwise around the player
+# the lines of the crosshair will be the starting trajectory of the bullets when they spawn
+# four bullets should spawn around the player and move along the crosshair when the player presses space
+
+class Bullet:
+    def __init__(self, pos, vel, radius):
+        self.pos = pos
+        self.vel = vel
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "#FF0000", self.pos, self.radius)
+
+    def move(self, dt):
+        self.pos[0] += self.vel[0] * dt
+        self.pos[1] += self.vel[1] * dt
+
+    def check_bounds(self, screen):
+        if self.pos[0] < 0 or self.pos[0] > screen.get_width() or self.pos[1] < 0 or self.pos[1] > screen.get_height():
+            return True
+        return False
+
+    def check_collision(self, player_pos):
+        if ((self.pos[0] - player_pos[0]) ** 2 + (self.pos[1] - player_pos[1]) ** 2) ** 0.5 < self.radius + PLAYER_RADIUS:
+            return True
+        return False
+    
+    def bounce(self):
+        self.vel[0] *= -1
+        self.vel[1] *= -1
+    
--- game.py
+++ game.py
@@ -1,15 +1,45 @@
 # Example file showing a basic pygame "game loop"
 import pygame
+from bullet import Bullet
+
+# constants
+SHOW_CONTROLS = True
+BORDER_WIDTH = 20
+PLAYER_RADIUS = 5
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+TEXT_COLOR = "#bc7ad6"
+BG_COLOR = "#443554"
+BORDER_COLOR = "#19151c"
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-BORDER_WIDTH = 20
-
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+# bullets
+bullets = []
+BULLET_SPEED = 300
+
+def spawn_bullet():
+    angle_step = 360 // 4
+    for angle in range(0, 360, angle_step):
+        bullet_dir = pygame.Vector2(1, 0).rotate(angle)
+        bullet_pos = player_pos + bullet_dir * (PLAYER_RADIUS + 10)
+        bullet = Bullet(bullet_pos, bullet_dir * BULLET_SPEED, 3)
+        bullets.append(bullet)
+
+def handle_bullets():
+    for bullet in bullets:
+        bullet.move(dt)
+        if bullet.check_bounds(screen):
+            bullet.bounce()
+        if bullet.check_collision(player_pos):
+            bullets.remove(bullet)
+
 
 
 
@@ -30,40 +60,94 @@ def check_bounds():
         return True
     return False
 
+# check if player is holding shift
+def check_sprint():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LSHIFT or pygame.K_RSHIFT]:
+        return True
+    return False
+
 def handle_movement():
+    global SHOW_CONTROLS
     if check_bounds():
         return
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w or pygame.K_UP]:
-        player_pos.y -= 300 * dt
+        if check_sprint():
+            player_pos.y -= 500 * dt
+        else: 
+            player_pos.y -= 300 * dt
+        SHOW_CONTROLS = False
     if keys[pygame.K_s or pygame.K_DOWN]:
-        player_pos.y += 300 * dt
+        if check_sprint():
+            player_pos.y += 500 * dt
+        else:
+            player_pos.y += 300 * dt
+        SHOW_CONTROLS = False
     if keys[pygame.K_a or pygame.K_LEFT]:
-        player_pos.x -= 300 * dt
+        if check_sprint():
+            player_pos.x -= 500 * dt
+        else:
+            player_pos.x -= 300 * dt
+        SHOW_CONTROLS = False
     if keys[pygame.K_d or pygame.K_RIGHT]:
-        player_pos.x += 300 * dt
+        if check_sprint():
+            player_pos.x += 500 * dt
+        else:
+            player_pos.x += 300 * dt
+        SHOW_CONTROLS = False
+
+
+
+
+
+
+
+
+
+
+    
+# draw the screen, border, and player
+def draw_screen():
+    screen.fill(BG_COLOR)
+    pygame.draw.rect(screen, BORDER_COLOR, (0, 0, screen.get_width(), screen.get_height()), BORDER_WIDTH)
+    pygame.draw.circle(screen, BORDER_COLOR, player_pos, PLAYER_RADIUS)
+
+    for bullet in bullets:
+        bullet.draw(screen)
+    
+    if SHOW_CONTROLS:
+        font = pygame.font.Font("./font.ttf", 20)
+        text = font.render("WASD to move", True, "#FFFFFF")
+        text_rect = text.get_rect(center=(SCREEN_WIDTH / 2, 40))
+        screen.blit(text, text_rect)
+        text = font.render("Shift to sprint", True, "#FFFFFF")
+        text_rect = text.get_rect(center=(SCREEN_WIDTH / 2, 60))
+        screen.blit(text, text_rect)
 
 # game loop
 while running:
-    # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # check if the user pressed a key
+        if event.type == pygame.KEYDOWN:
+            # check if the user pressed space
+            if event.key == pygame.K_SPACE:
+                spawn_bullet()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("#443554")
-    # create a border around the screen
-    pygame.draw.rect(screen, "#19151c", (0, 0, screen.get_width(), screen.get_height()), BORDER_WIDTH)
-    pygame.draw.circle(screen, "#19151c", player_pos, 4)
-
+        
+    # draw the screen
+    draw_screen()
+    # handle player movement
     handle_movement()
+    # handle bullets
+    handle_bullets()
 
-    # RENDER YOUR GAME HERE
 
-    # flip() the display to put your work on screen
+    # render the screen
     pygame.display.flip()
-
     dt = clock.tick(60) / 1000 # limits FPS to 60
 
 pygame.quit()
