ad93493bdbf9dfffe8d1088f582b29d00be4780d
apolyeti <azhandarveen@gmail.com>
2023-07-07 17:11:53 -0700

    add a border, and make sure player cannot move past it

---
+++ README.md
@@ -0,0 +1,7 @@
+# Survive Your Own Disaster
+
+## Description
+
+simple pygame where you survive your own disaster.
+press space to spawn in more bullets to dodge. the longer you survive, the more points you get.
+point multiplier increases the more bullets you spawn in.
--- game.py
+++ game.py
@@ -7,20 +7,43 @@ screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+BORDER_WIDTH = 20
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-def check_boundary(pos):
-    if pos.x < 0:
-        pos.x = 0
-    elif pos.x > screen.get_width():
-        pos.x = screen.get_width()
-    if pos.y < 0:
-        pos.y = 0
-    elif pos.y > screen.get_height():
-        pos.y = screen.get_height()
 
 
+# also make sure to check if player is out of bounds
+def check_bounds():
+    bound_width = BORDER_WIDTH + 5
+    if player_pos.x < bound_width:
+        player_pos.x = bound_width
+        return True
+    if player_pos.x > screen.get_width() - bound_width:
+        player_pos.x = screen.get_width() - bound_width
+        return True
+    if player_pos.y < bound_width:
+        player_pos.y = bound_width
+        return True
+    if player_pos.y > screen.get_height() - bound_width:
+        player_pos.y = screen.get_height() - bound_width
+        return True
+    return False
+
+def handle_movement():
+    if check_bounds():
+        return
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w or pygame.K_UP]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s or pygame.K_DOWN]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a or pygame.K_LEFT]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d or pygame.K_RIGHT]:
+        player_pos.x += 300 * dt
+
+# game loop
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -30,20 +53,11 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("#443554")
-
+    # create a border around the screen
+    pygame.draw.rect(screen, "#19151c", (0, 0, screen.get_width(), screen.get_height()), BORDER_WIDTH)
     pygame.draw.circle(screen, "#19151c", player_pos, 4)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w or pygame.K_UP]:
-        if keys[pygame.K_LSHIFT]:
-            player_pos.y -= 600 * dt
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s or pygame.K_DOWN]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a or pygame.K_LEFT]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d or pygame.K_RIGHT]:
-        player_pos.x += 300 * dt
+    handle_movement()
 
     # RENDER YOUR GAME HERE
 
