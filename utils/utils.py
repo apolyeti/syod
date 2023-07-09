386bc806f0531b5e39a4f5512a35d0d10335fe07
apolyeti <azhandarveen@gmail.com>
2023-07-09 15:39:42 -0700

    move globals to another file

--- utils/utils.py
+++ utils/utils.py
@@ -1,33 +1,12 @@
 import pygame
 import math
 import random
+
+# import bullet class
 from utils.bullet import Bullet
+# import globals
+from utils.globals import *
 
-# global variables
-
-SHOW_CONTROLS = True
-BORDER_WIDTH = 20
-PLAYER_RADIUS = 7
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-FONT = "utils/font.ttf"
-FONT_COLOR = "#bc7ad6"
-BG_COLOR = "#443554"
-BORDER_COLOR = "#19151c"
-ROTATION_SPEED = 500
-POINTS = 0
-POINT_MULTIPLIER = 0.9 # 1.0 when game begins
-CURR_ANGLE = 0
-LIVES = 5
-MAX_POINTS = 1000
-INVINCIBLE_TIME = 1500
-INVINCIBLE_TIMER = 0
-INVINCIBLE = False
-SHAKE_DURATION = 100
-SHAKE_MAG = 7
-SHAKE_TIMER = 0
-SHAKE_OFFSET = pygame.Vector2(0, 0)
-BULLET_SPEED = 300
 
 # set up display and player position for util functions
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -42,6 +21,7 @@ def update_dt(new_dt):
     global dt
     dt = new_dt
 
+# check if player has hit the border or not
 def check_bounds():
     bound_width = BORDER_WIDTH + 5
     if player_pos.x < bound_width:
@@ -58,6 +38,7 @@ def check_bounds():
         return True
     return False
 
+# draw 4 bullets surrounding player
 def spawn_bullet():
     global CURR_ANGLE, POINT_MULTIPLIER, MAX_POINTS
     POINT_MULTIPLIER += 0.1
@@ -75,6 +56,7 @@ def spawn_bullet():
         bullet.rotate(math.radians(current_angle))
         CURR_ANGLE += ROTATION_SPEED * dt
 
+# move bullets and check if they have hit the player
 def handle_bullets():
     global LIVES, INVINCIBLE, INVINCIBLE_TIME, INVINCIBLE_TIMER, SHAKE_TIMER, SHAKE_MAG
     for bullet in bullets:
@@ -99,6 +81,7 @@ def handle_bullets():
         if INVINCIBLE and pygame.time.get_ticks() - INVINCIBLE_TIMER > INVINCIBLE_TIME:
             INVINCIBLE = False
 
+# if shift pressed move faster
 def check_sprint():
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LSHIFT or pygame.K_RSHIFT]:
@@ -136,6 +119,7 @@ def handle_movement():
             else:
                 player_pos.x += 300 * dt
 
+# restart game
 def reinit():
     global POINTS, POINT_MULTIPLIER, CURR_ANGLE, LIVES, SHAKE_MAG, SHOW_CONTROLS, player_pos
     POINTS = 0
@@ -190,7 +174,8 @@ def draw_screen():
             else:
                 SHAKE_OFFSET = pygame.Vector2(0, 0)
                 SHAKE_TIMER = 0
-        
+
+# game over screen        
 def draw_game_over():
         screen.fill(BG_COLOR)
         font = pygame.font.Font(FONT, 30)
